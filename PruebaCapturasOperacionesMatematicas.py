0ec87091429e924a5e1078eebe2e81ddd5c8cd28
wescot2000 <wescot2000@gmail.com>
2021-01-13 14:59:42 -0500

    Update
    UpdateEnero

---
+++ PruebaCapturasOperacionesMatematicas.py
@@ -0,0 +1,19 @@
+#%%
+print("Hello world")
+
+#%%
+import math
+#import cmath;
+
+
+c=float(input("Digite primer valor:"))
+b=float(input("Digite segundo valor:"))
+a=c*b
+print(a)
+print("Multiplicado: ",c*b)
+print("Restado: ",c-b)
+print("Sumado: ",c+b)
+print("Dividido: ",c/b)
+print("Elevado: ",pow(b,c))
+print("Logaritmo: ",(math.log(c,b)))
+# %%
